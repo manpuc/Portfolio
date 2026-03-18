ecc616a5591e5f379ae9ad2470f0e984de104b2d
manpuc <guangxitengchuan@gmail.com>
2026-03-18 17:20:55 +0900

    feat: Implement base layout, global styling with dark mode, and a custom 403 error page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,20 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 
+
 export default defineConfig({
   site: 'https://manpuc.me',
   integrations: [react(), sitemap()],
+    vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {}  // SCSSを使いたい場合
+      }
+    },
+    build: {
+      cssCodeSplit: true  // ページごとにCSS分割（重要）
+    }
+  },
   adapter: vercel({
     entrypointResolution: 'auto'
   })
