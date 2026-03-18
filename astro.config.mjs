0cb7b0447da7cf52eb4396da7199c9d0ce2c0457
manpuc <guangxitengchuan@gmail.com>
2026-03-18 12:51:20 +0900

    Remove build and Vite configurations for stylesheet inlining and font chunking.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,11 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://manpuc.me',
   integrations: [react(), sitemap()],
   adapter: vercel(),
-  build: {
-    inlineStylesheets: 'always',
-  },
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('@fontsource')) {
-              return 'fonts';
-            }
-          }
-        }
-      }
-    }
-  }
 });
