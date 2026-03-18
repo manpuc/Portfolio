ebf5e8e080a2916fbae14c2236a8956c919701eb
manpuc <guangxitengchuan@gmail.com>
2026-03-18 12:33:35 +0900

    feat: Configure Astro to inline stylesheets and chunk fonts using Vite.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
-import vercel from '@astrojs/vercel';
-
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://manpuc.me',
   integrations: [react(), sitemap()],
-  adapter: vercel()
+  adapter: vercel(),
+  build: {
+    inlineStylesheets: 'always',
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('@fontsource')) {
+              return 'fonts';
+            }
+          }
+        }
+      }
+    }
+  }
 });
